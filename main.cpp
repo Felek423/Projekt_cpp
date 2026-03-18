d59f62de0ca95ed6eeb961d405c8672288e13264
Bartlomiej <bartekxdjd@gmail.com>
2026-03-18 04:21:16 +0100

    wiekszy pokoj z obramowaniem, wysrodkowany

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include "raylib.h"
 
 int main(){
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+    int screenWidth = 800;
+    int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Lochy");
-    ToggleFullscreen();
     SetTargetFPS(30);
 
     //gracz
     Vector2 playerPos = {400, 300}; //pozycja gracza
     float playerSpeed = 200.0f; //szybkość gracza
     float playerSize = 20.0f; //rozmiar gracza
+    screenWidth = GetMonitorWidth(GetCurrentMonitor());
+    screenHeight = GetMonitorHeight(GetCurrentMonitor());    
+    ToggleFullscreen();
+
+
+    // definiowanie pokoju
+        float roomWidth = 1000;
+        float roomHeight = 600;
+        float roomX = (screenWidth - roomWidth) / 2.0f;
+        float roomY = (screenHeight - roomHeight) / 2.0f;
 
     while(!WindowShouldClose()){
 
@@ -20,20 +29,16 @@ int main(){
         if(IsKeyDown(KEY_A)) playerPos.x -= playerSpeed * GetFrameTime(); 
         if(IsKeyDown(KEY_D)) playerPos.x += playerSpeed * GetFrameTime(); 
 
-        // definiowanie pokoju
-        float roomWidth = 600.0f;
-        float roomHeight = 400.0f;
-        float roomX = (screenWidth - roomWidth) / 2.0f;
-        float roomY = (screenHeight - roomHeight) / 2.0f;
-
         //ograniczenie ruchu gracza do obszaru pokoju
-        if (playerPos.x - playerSize <= roomX) playerPos.x = roomX + playerSize;
-        if (playerPos.x + playerSize >= roomX + roomWidth) playerPos.x = roomX + roomWidth - playerSize;
-        if (playerPos.y - playerSize <= roomY) playerPos.y = roomY + playerSize;
-        if (playerPos.y + playerSize >= roomY + roomHeight) playerPos.y = roomY + roomHeight - playerSize;  
+        if (playerPos.x - playerSize <= roomX) playerPos.x = roomX + playerSize + 5;
+        if (playerPos.x + playerSize >= roomX + roomWidth) playerPos.x = roomX + roomWidth - playerSize - 5;
+        if (playerPos.y - playerSize <= roomY) playerPos.y = roomY + playerSize + 5;
+        if (playerPos.y + playerSize >= roomY + roomHeight) playerPos.y = roomY + roomHeight - playerSize - 5;
 
         BeginDrawing();
         DrawRectangle(roomX, roomY, roomWidth, roomHeight, GREEN); //rysowanie pokoju
+        Rectangle roomRect = {roomX, roomY, roomWidth, roomHeight}; //definiowanie prostokąta pokoju
+        DrawRectangleLinesEx(roomRect, 5, DARKGREEN); //rysowanie obramowania pokoju
         DrawCircleV(playerPos, playerSize, BLUE); //rysowanie gracza
         ClearBackground(GRAY);
         EndDrawing();
