30d97038472e5deeb1849ab12509b4ba86bbc56f
Bartlomiej <bartekxdjd@gmail.com>
2026-03-18 03:04:50 +0100

    dodanie okna pokoju i ograniczenie ruchu do wielkosci pokoju

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main(){
     const int screenWidth = 800;
     const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "Lochy");
+    ToggleFullscreen();
     SetTargetFPS(30);
 
     //gracz
@@ -19,14 +20,20 @@ int main(){
         if(IsKeyDown(KEY_A)) playerPos.x -= playerSpeed * GetFrameTime(); 
         if(IsKeyDown(KEY_D)) playerPos.x += playerSpeed * GetFrameTime(); 
 
-        //ograniczenie ruchu gracza do obszaru ekranu
-        if(playerPos.x < playerSize) playerPos.x = playerSize;
-        if(playerPos.x > screenWidth - playerSize) playerPos.x = screenWidth - playerSize; 
-        if(playerPos.y < playerSize) playerPos.y = playerSize; 
-        if(playerPos.y > screenHeight - playerSize) playerPos.y = screenHeight - playerSize; 
+        // definiowanie pokoju
+        float roomWidth = 600.0f;
+        float roomHeight = 400.0f;
+        float roomX = (screenWidth - roomWidth) / 2.0f;
+        float roomY = (screenHeight - roomHeight) / 2.0f;
+
+        //ograniczenie ruchu gracza do obszaru pokoju
+        if (playerPos.x - playerSize <= roomX) playerPos.x = roomX + playerSize;
+        if (playerPos.x + playerSize >= roomX + roomWidth) playerPos.x = roomX + roomWidth - playerSize;
+        if (playerPos.y - playerSize <= roomY) playerPos.y = roomY + playerSize;
+        if (playerPos.y + playerSize >= roomY + roomHeight) playerPos.y = roomY + roomHeight - playerSize;  
 
         BeginDrawing();
-
+        DrawRectangle(roomX, roomY, roomWidth, roomHeight, GREEN); //rysowanie pokoju
         DrawCircleV(playerPos, playerSize, BLUE); //rysowanie gracza
         ClearBackground(GRAY);
         EndDrawing();
